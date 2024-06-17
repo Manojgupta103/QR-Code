08df3e4110c8c55aa90511749df0e745b8213e63
Manoj <manoj.gupta16539@sakec.ac.in>
2024-06-17 23:55:25 +0530

    QR CODE gennerated for Web page

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,18 @@ import './App.css'
 function App() {
   return (
     <>
-      <div className='Main-box'>
-
+      <div className='main'>
+        <div className='box'>
+          <div className='qr'>
+            <div className='image'>
+              <img src='./images/qr-code.png' alt='QR Code' />
+            </div>
+          </div>
+          <div className='text'>
+            <h1>Manoj Gupta's Profile</h1>
+            <p>Above is the qr code for the profile of Manoj Gupta's</p>
+          </div>
+        </div>
       </div>
     </>
   )
